d78181a1f7429b174f0d33364ef4bec90ab30a0a
prabhat-cn <prabhat@capitalnumbers.com>
2022-01-11 16:15:00 +0530

    first commit

---
+++ src/common/apis/MovieApiKey.js
@@ -0,0 +1,10 @@
+export const APIKey = '440c4a62';
+
+// Here is your key: 440c4a62
+
+// Please append it to all of your API requests,
+
+// OMDb API: http://www.omdbapi.com/?i=tt3896198&apikey=440c4a62
+
+// Click the following URL to activate your key: http://www.omdbapi.com/apikey.aspx?VERIFYKEY=81702620-b194-46ad-b942-1e79594c605f
+// If you did not make this request, please disregard this email.
